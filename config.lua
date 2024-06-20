4edd75edab1268e7480a56b8972f48dc161582a0
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-06-19 23:42:24 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,27 @@
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "vim"
 
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+lvim.lang.python.formatters = {
+  {
+    exe = "black",
+  }
+}
+
+lvim.lang.python.linters = {
+  {
+    exe = "flake8",
+  }
+}
